336986bbd8556145f1dd9fc0c757adb4ee8ed9c4
sana arshed <sana.arshed94@gmail.com>
2025-06-02 00:51:17 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Tailwind + Vite = ✅
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
